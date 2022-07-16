48209162e4a52c095730b849a9d3ec01e60c2545
Conner <conner.goldberg@gmail.com>
2022-07-16 16:30:45 +1000

    install eleventy and add the blog njk timestamp 28:05

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/styles.css");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/normalize.css");
+  eleventyConfig.addPassthroughCopy("./src/application.css");
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};
