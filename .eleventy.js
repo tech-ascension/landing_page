d4a4385457308030613a5c5d6da07278c3e9dfc1
Conner <conner.goldberg@gmail.com>
2022-07-17 03:06:37 +1000

    add the blog base template and the blog header

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/styles.css");
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/normalize.css");
+  eleventyConfig.addPassthroughCopy("./src/blog.css");
   eleventyConfig.addPassthroughCopy("./src/application.css");
 
   return {
