3d8d9e973f37d2cda937481dc3d94c5c0ec30966
DESKTOP-KEK5N84\61424 <chriszhang@propic.com.au>
2022-08-10 17:03:33 +1000

    refine the post page and adjust the icon center in the circle

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,17 @@
-const {DateTime} = require("luxon");
-
+const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/styles.css");
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/normalize.css");
-  eleventyConfig.addPassthroughCopy("./src/blog.css");
+  // eleventyConfig.addPassthroughCopy("./src/blog.css");
   eleventyConfig.addPassthroughCopy("./src/application.css");
   eleventyConfig.addPassthroughCopy("./src/admin");
 
   // DateTime Configuration
 
-  eleventyConfig.addFilter("postDate", (dateObj)=>{
-    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
 
   return {
