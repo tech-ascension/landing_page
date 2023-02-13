8a71f26a5f7af6ab6f19d79bfb6e9a7f18b6bafc
conner-a2x <114885438+conner-a2x@users.noreply.github.com>
2023-02-13 15:26:19 +1100

    reorder the blog content

--- .eleventy.js
+++ .eleventy.js
@@ -13,6 +13,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
+  eleventyConfig.addFilter("date", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
 
   return {
     dir: {
