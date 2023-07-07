75141dd9533a3830423829975f90bb4a40db4022
pexeixv <gavineditz@gmail.com>
2023-07-07 15:33:24 +0530

    hygraph and recaptcha integration

--- .eleventy.js
+++ .eleventy.js
@@ -9,15 +9,26 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addFilter('makeExcerpt', (description) => description.slice(0, 300).trim() + '...');
   eleventyConfig.addFilter('slice', (array, number) => array.slice(0, number));
-
-  // DateTime Configuration
-
-  eleventyConfig.addFilter('postDate', (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  eleventyConfig.addFilter('removeEscapes', (str) => eval('`' + str + '`'));
+  eleventyConfig.addFilter('removeFirstItem', (array) => {
+    if (Array.isArray(array) && array.length > 0) {
+      return array.slice(1);
+    }
+    return array;
   });
-  eleventyConfig.addFilter('date', (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+
+  eleventyConfig.addFilter('postDate', (dateString) => {
+    const options = { year: 'numeric', month: 'short', day: 'numeric' };
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-US', options);
   });
+  eleventyConfig.addFilter('date', (dateObj) => DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED));
+
+  eleventyConfig.addFilter('recentPosts', (posts, id) => posts.filter((post) => post.id != id));
+
+  eleventyConfig.addFilter('filterFeaturedPosts', (posts) => posts.filter((post) => post.featured));
+
+  eleventyConfig.addFilter('recentPosts', (posts, id) => posts.filter((post) => post.id != id));
 
   return {
     dir: {
